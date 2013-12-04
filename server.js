7c73f8db6a4cf6c12bec0bf279e10058bd4da7cd
Garrett Wilkin <garrett.wilkin@gmail.com>
2013-12-04 12:48:15 -0500

    First draft adding analytics to API examples. Much better use of bootstrap form styling. Preparing to do server side API key requests.

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ var http = require('http');
 var static = require('node-static');
 var file = new(static.Server)('.');
 
+var host = 'localhost';
+var port = 8080;
+var API_KEY = process.env.API_KEY;
+var API_SECRET = process.env.API_SECRET;
 // Create HTTP server
 var serv = http.createServer(function (req, res) {
-  console.log('HTTP Connection');
+  console.log('HTTP request');
   req.addListener('end', function () {
     // Serve files!
     file.serve(req, res);
   }).resume();
-}).listen(8080);
-console.log('Server running at http://localhost:8080');
+}).listen(port,host);
+console.log('Server running at http://'+host+':'+port);
+console.log('API KEY: '+API_KEY);
+console.log('API SECRET: '+API_SECRET);
