f3023e466326fb34b68e5cfa66dcb75e75f1a224
Garrett Wilkin <garrett.wilkin@gmail.com>
2013-12-04 16:57:35 -0500

    adding interesting parameters that theroot wanted to investigate for the recommendations page.  webserver is ready to route requests and hit parsely. clicks.method input field is only shown if strategy is not recency.

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
-var http = require('http');
-var static = require('node-static');
+var http = require('http'),
+    director = require('director'),
+    static = require('node-static');
 var file = new(static.Server)('.');
 
-var host = 'localhost';
-var port = 8080;
-var API_KEY = process.env.API_KEY;
-var API_SECRET = process.env.API_SECRET;
+var host = 'localhost',
+    port = 8080,
+    API_KEY = process.env.API_KEY,
+    API_SECRET = process.env.API_SECRET;
+
+function hitParsely() {
+  this.res.writeHead(200, {'Content-Type': 'text/plain' });
+  this.res.end('hitting parsely');
+}
+
+// define a routing table.
+var router = new director.http.Router({
+  '/data': {
+    get: hitParsely
+  }
+});
+
 // Create HTTP server
 var serv = http.createServer(function (req, res) {
-  console.log('HTTP request');
+  console.log('HTTP request: '+ req.url);
   req.addListener('end', function () {
     // Serve files!
-    file.serve(req, res);
+    if (req.url != '/data') {
+      file.serve(req, res);
+    } else {
+      router.dispatch(req, res, function(err) {
+        if (err) {
+          res.writeHead(404);
+          res.end()
+        }
+      });
+    }
   }).resume();
 }).listen(port,host);
 console.log('Server running at http://'+host+':'+port);
