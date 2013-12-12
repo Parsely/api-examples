5223d6724ceb57ba9aa6e76fd9126faa41dd01da
Garrett Wilkin <garrett.wilkin@gmail.com>
2013-12-11 23:01:12 -0500

    Added lots of fields to the analytics page. Got requests to the node server working. Failing when attempting to process the request.

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require('http'),
     director = require('director'),
+    url = require('url'),
+    querystring = require('querystring'),
+    request = require('request'),
     static = require('node-static');
 var file = new(static.Server)('.');
 
@@ -8,24 +11,43 @@ var host = 'localhost',
     API_KEY = process.env.API_KEY,
     API_SECRET = process.env.API_SECRET;
 
-function hitParsely() {
+var baseUrl = "http://api.parsely.com/v2";
+
+function analytics() {
   this.res.writeHead(200, {'Content-Type': 'text/plain' });
-  this.res.end('hitting parsely');
+  var query = url.parse(this.req.url,true).query;
+  delete query.callback;
+  delete query._;
+  var qs = querystring.stringify(query);  
+  var options = { uri: baseUrl, qs: qs};
+  request(options,function(err,res,body) {
+    that = this;
+    if (err) {
+      that.res.end('Error - Derp threshold breach');
+      console.log('request callback error');
+    } else {
+      that.res.end(JSON.stringify(body));
+    }
+  });
 }
 
 // define a routing table.
 var router = new director.http.Router({
-  '/data': {
-    get: hitParsely
+  '/v2/analytics': {
+    get: analytics
   }
 });
 
 // Create HTTP server
 var serv = http.createServer(function (req, res) {
-  console.log('HTTP request: '+ req.url);
+  console.log('HTTP request: req.method:  '+ req.method);
+  console.log('HTTP request: req.url:  '+ req.url);
+  console.log('HTTP request: req.body: '+ req.body);
+  var path = url.parse(req.url,true).pathname;
+  console.log('HTTP request: path: '+ path);
   req.addListener('end', function () {
     // Serve files!
-    if (req.url != '/data') {
+    if (path != '/v2/analytics') {
       file.serve(req, res);
     } else {
       router.dispatch(req, res, function(err) {
